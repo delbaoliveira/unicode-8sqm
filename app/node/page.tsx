0249d7c9c8c22956e9d241963bb8e8e223174ce9
Delba de Oliveira <delba.brown@gmail.com>
2023-02-24 20:17:53 +0000

    Create page.tsx

---
+++ app/node/page.tsx
@@ -0,0 +1,13 @@
+export default async function Home() {
+  const data = await fetch("https://unicode-8sqm.vercel.app/api/hello").then(
+    (res) => res.json(),
+  )
+
+  return (
+    <div>
+      <h1>{data.jp}</h1>
+      <h2>{data.course?.name}</h2>
+      <h2>{data.course?.description}</h2>
+    </div>
+  )
+}
